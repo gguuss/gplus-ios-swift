aafd808b56118e6bf119a34ffb622a0bd51a04b5
Gus Class <gguuss@gmail.com>
2015-02-01 01:48:18 -0800

    Various cleanup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# gplus-ios-swift
+# Google+ iOS Demo using Swift and a Bridging Header
 Demonstration of how to use a bridging header for Google+ Sign-In (iOS and Swift).
+
+# Setup
+
+Get a client ID from https://console.deveopers.google.com.
+For the bundle ID, set it to com.gclassy.swiftsignin - if you want to change this
+modify the bundle identifier in the project first. Also, you will need to update
+the Scheme defined in the URL handlers in info.plist.
--- basic-signin/swiftsignin/ViewController.swift
+++ basic-signin/swiftsignin/ViewController.swift
@@ -3,54 +3,111 @@
 //  swiftsignin
 //
 //  Created by Gus Class on 12/12/14.
-//  Copyright (c) 2014 gclassy. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 import UIKit
 
 
 class ViewController: UIViewController, GPPSignInDelegate {
     @IBOutlet weak var toggleFetchEmail: UISwitch!
-    @IBOutlet weak var signinButton: UIButton!
     @IBOutlet weak var toggleFetchUserID: UISwitch!
-    @IBOutlet weak var userData: UITextView!
+    
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    
     @IBOutlet weak var emailDataField: UITextView!
+    @IBOutlet weak var userData: UITextView!
 
     override func viewDidLoad() {
-        super.viewDidLoad();
+      super.viewDidLoad();
 
-        // Do any additional setup after loading the view, typically from a nib.
-        var signIn = GPPSignIn.sharedInstance();
-        signIn.shouldFetchGooglePlusUser = true;
-        signIn.clientID = "YOUR_CLIENT_ID"; // Get this from https://console.developers.google.com
-        signIn.shouldFetchGoogleUserEmail = toggleFetchEmail.enabled;
-        signIn.shouldFetchGoogleUserID = toggleFetchUserID.enabled;
-        signIn.scopes = [kGTLAuthScopePlusLogin];
-        signIn.trySilentAuthentication();
-        signIn.delegate = self;
+      // Configure the sign in object.
+      var signIn = GPPSignIn.sharedInstance();
+      signIn.shouldFetchGooglePlusUser = true;
+      signIn.clientID = "REPLACE_CLIENT_ID"; // Get this from https://console.developers.google.com
+      signIn.shouldFetchGoogleUserEmail = toggleFetchEmail.on;
+      signIn.shouldFetchGoogleUserID = toggleFetchUserID.on;
+      signIn.scopes = [kGTLAuthScopePlusLogin];
+      signIn.trySilentAuthentication();
+      signIn.delegate = self;
+
+      // Update the buttons and text.
+      updateUI();
     }
 
+    
     @IBAction func signInClicked(sender: AnyObject) {
-        var signIn = GPPSignIn.sharedInstance();
-        signIn.authenticate();
+      var signIn = GPPSignIn.sharedInstance();
+      signIn.authenticate();
     }
 
+    
     func finishedWithAuth(auth: GTMOAuth2Authentication!, error: NSError!) {
-        var signIn = GPPSignIn.sharedInstance();
-        var user = signIn.googlePlusUser;
-        var str = "User ID: " + user.JSONString();
-        //NSLog(str);
-        NSLog(user.name.JSONString());
-        NSLog(user.emails.first?.JSONString() ?? "no email");
-        emailDataField.text = user.emails.first?.JSONString() ?? "no email";
-        userData.text = user.name.JSONString();
+      updateUI();
     }
 
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func toggleFetchEmailClick(sender: AnyObject) {
+      GPPSignIn.sharedInstance().shouldFetchGoogleUserEmail = toggleFetchEmail.on;
+    }
 
+    
+    @IBAction func toggleUserId(sender: AnyObject) {
+      GPPSignIn.sharedInstance().shouldFetchGoogleUserID = toggleFetchUserID.on;
+    }
+    
+
+    @IBAction func disconnect(sender: AnyObject) {
+      GPPSignIn.sharedInstance().disconnect();
+      updateUI();
+    }
+    
+    
+    @IBAction func signOut(sender: AnyObject) {
+        GPPSignIn.sharedInstance().signOut();
+        updateUI();
+    }
+    
+    func updateUI() {
+      // TODO: Toggle buttons here.
+      if (GPPSignIn.sharedInstance().userID != nil){
+        // Signed in?
+        var user = GPPSignIn.sharedInstance().googlePlusUser;
+        userData.text = user.name.JSONString();
+        if (user.emails != nil){
+          emailDataField.text = user.emails.first?.JSONString() ?? "no email";
+        } else {
+          emailDataField.text = "no email";
+        }
+        
+        signOutButton.enabled = true;
+        disconnectButton.enabled = true;
+        signinButton.enabled = true;
+      } else {
+          userData.text = "Signed out.";
+          emailDataField.text = "Signed out.";
+          signOutButton.enabled = false;
+          disconnectButton.enabled = false;
+          signinButton.enabled = true;
+      }
+    }
 }
 
